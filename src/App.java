0bf56270be90a1043c1d6e9393cd3bed2c24861d
Steven Pickering <52013044+Steven909@users.noreply.github.com>
2021-10-27 16:00:15 +0100

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        final char ACCOUNT_A = 'A';
+        final char ACCOUNT_B = 'B';
+        final char ACCOUNT_C = 'C';
+        final char ACCOUNT_X = 'X';
+
+        double bankAInterestRate = 1.5;
+        double bankBInterestRate = 2;
+        double bankCInterestRate = 1.5;
+        double bankXInterestRate = 5;
+
+        System.out.println("Enter amount of money > ");
+        Scanner scanner = new Scanner(System.in);
+        int amount = scanner.nextInt();
+
+        System.out.println("Select bank account A, B, C or X and press enter");
+
+        char bankAccount = scanner.next().charAt(0);
+
+
+        switch (bankAccount) {
+
+            case ACCOUNT_A:
+                System.out.println("Account A ");
+                break;
+            case ACCOUNT_B:
+                System.out.println("Account B ");
+                break;
+            case ACCOUNT_C:
+                System.out.println("Account C ");
+                break;
+            case ACCOUNT_X:
+                System.out.println("Account X ");
+                break;
+            default:
+                System.out.println("No account selected");
+        }
+
+    }
+}
+
